d9d0cde89e61870e539e1b3155838a85ae698e8d
wjddms0501 <wjddmswlsgh@naver.com>
2022-11-26 15:14:54 +0900

    React-Week-1-Practice_01
    Practice

---
+++ src/Components/CustomButton.js
@@ -0,0 +1,14 @@
+function CustomButton(props) {
+  const { color, onClick, children } = props;
+  if (color) {
+    return (
+      <button
+        style={{ backgroundColor: color, color: "white" }}
+        onClick={onClick}
+      >
+        {children}
+      </button>
+    );
+  }
+  return <button onClick={onClick}>{children}</button>;
+}
---
+++ src/Components/User.js
@@ -0,0 +1,15 @@
+import CustomButton from "./components/CustomButton";
+
+function User(props) {
+    return (
+      <div className="square-style">
+        {props.user.age}살 - {props.user.name}
+        📍👉<CustomButton color="red" onClick={()=>{props.handleDelete(props.user.id)}}>삭제하기</CustomButton>
+      {/*👆CustomButton의 props로 color 넘기기 */}
+      {/*👆삭제버튼 */}
+      </div>
+    );
+  }
+  
+  export default User
+  export default CustomButton
